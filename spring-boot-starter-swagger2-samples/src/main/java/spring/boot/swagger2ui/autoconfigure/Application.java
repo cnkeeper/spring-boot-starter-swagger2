b64021f9e4f9db052f7f02d9fb57d8268cbb391b
TIME69 <1348555156@qq.com>
2018-08-12 16:12:07 +0800

    complete

---
+++ spring-boot-starter-swagger2-samples/src/main/java/spring/boot/swagger2ui/autoconfigure/Application.java
@@ -0,0 +1,20 @@
+package spring.boot.swagger2ui.autoconfigure;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述~
+ *
+ * @author <a href="1348555156@qq.com">LeiLi.Zhang</a>
+ * @version 0.0.0
+ * @date 2018/8/12
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            SpringApplication application = new SpringApplication(Application.class);
+            application.run(args);
+        }
+    }
+}
